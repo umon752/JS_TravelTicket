d0652075461a00693a0568230020cfc9433b58bc
umon752 <umon752@gmail.com>
2021-03-18 18:05:47 +0800

    修改與新增驗證顯示文字

--- assets/js/all.js
+++ assets/js/all.js
@@ -49,7 +49,16 @@ const card = document.querySelector(".js-cardList");
 const selectArea = document.querySelector(".js-searchArea");
 const searchCount = document.querySelector(".js-searchCount");
 const addTicket = document.querySelector(".js-addTicket");
-// const from = document.querySelector(".js-from");
+const form = document.querySelector(".js-form");
+
+const ticketName = document.querySelector(".js-ticketName");
+const ticketImg = document.querySelector(".js-ticketImg");
+const ticketArea = document.querySelector(".js-ticketArea");
+const ticketPrice = document.querySelector(".js-ticketPrice");
+const ticketGroup = document.querySelector(".js-ticketGroup");
+const ticketTag = document.querySelector(".js-ticketTag");
+const ticketDescription = document.querySelector(".js-ticketDescription");
+const verify = document.querySelectorAll(".js-verify");
 
 
 
@@ -59,9 +68,17 @@ const addTicket = document.querySelector(".js-addTicket");
 // 初始化 繪製 card
 function init() {
     let str = "";
-    let aosDelay = 0;
     data.forEach(function (item, index) {
-        str += `<div class="col col-md-6 col-lg-4 position-relative mb-12" data-aos="fade-up" data-aos-delay="${aosDelay+index*100}">
+        str += render(item, index);
+    })
+    card.innerHTML = str;
+};
+init();
+
+// 渲染
+function render(item, index) {
+    let aosDelay = 0;
+    return `<div class="col col-md-6 col-lg-4 position-relative mb-12" data-aos="fade-up" data-aos-delay="${aosDelay+index*100}">
         <div class="tag bg-primary-light font-size-m text-white py-3 px-8">${item.area}</div>
         <div class="rounded-base bg-white shadow overflow-hidden h-100">
             <a href="#" class="d-block overflow-hidden hover-img-scale-bigger">
@@ -91,11 +108,7 @@ function init() {
             </div>
         </div>
         </div>`
-    })
-    card.innerHTML = str;
-    searchCount.innerHTML = `本次搜尋共 ${data.length} 筆資料`;
-};
-init();
+}
 
 
 
@@ -108,140 +121,110 @@ function selectFilter(e) {
     let count = 0;
     data.forEach(function (item, index) {
         if (e.target.value === item.area) {
-            str += `<div class="col col-md-6 col-lg-4 position-relative mb-12" data-aos="fade-up">
-            <div class="tag bg-primary-light font-size-m text-white py-3 px-8">${item.area}</div>
-            <div class="rounded-base bg-white shadow overflow-hidden h-100">
-                <a href="#" class="d-block overflow-hidden hover-img-scale-bigger">
-                    <img src="${item.imgUrl}" class="cardImg w-100">
-                </a>
-                <div class="cardContent position-relative">
-                    <div class="tag bg-primary text-white py-1 px-3">${item.rate}</div>
-                    <div class="d-flex flex-column justify-content-between h-100 px-8 pt-8 pb-6">
-                        <div>
-                        <h2 class="border-primary border-bottom-bold pb-1 mb-7">
-                            <a href="#"
-                                class="font-size-l text-primary hover-text-primary-light font-weight-bold">${item.name}</a>
-                        </h2>
-                        <p class="text-secondary mb-9">
-                        ${item.description}
-                        </p>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between">
-                            <p class="text-primary d-flex align-items-center">
-                                <span class="material-icons mr-2">
-                                    error
-                                </span>剩下最後 ${item.group} 組</p>
-                            <h3 class="text-primary font-size-xl font-weight-bold d-flex align-items-center">
-                                <span class="font-size-base font-weight-normal mr-1">TWD</span>${item.price}</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>`
+            str += render(item, index);
             count++;
         } else if (e.target.value === "全部地區") {
-            str += `<div class="col col-md-6 col-lg-4 position-relative mb-12" data-aos="fade-up">
-        <div class="tag bg-primary-light font-size-m text-white py-3 px-8">${item.area}</div>
-        <div class="rounded-base bg-white shadow overflow-hidden h-100">
-            <a href="#" class="d-block overflow-hidden hover-img-scale-bigger">
-                <img src="${item.imgUrl}" class="cardImg w-100">
-            </a>
-            <div class="cardContent position-relative">
-                <div class="tag bg-primary text-white py-1 px-3">${item.rate}</div>
-                <div class="d-flex flex-column justify-content-between h-100 px-8 pt-8 pb-6">
-                    <div>
-                    <h2 class="border-primary border-bottom-bold pb-1 mb-7">
-                        <a href="#"
-                            class="font-size-l text-primary hover-text-primary-light font-weight-bold">${item.name}</a>
-                    </h2>
-                    <p class="text-secondary mb-9">
-                    ${item.description}
-                    </p>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <p class="text-primary d-flex align-items-center">
-                            <span class="material-icons mr-2">
-                                error
-                            </span>剩下最後 ${item.group} 組</p>
-                        <h3 class="text-primary font-size-xl font-weight-bold d-flex align-items-center">
-                            <span class="font-size-base font-weight-normal mr-1">TWD</span>${item.price}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>`
+            str += render(item, index);
             count++;
         }
-        card.innerHTML = str;
-        searchCount.innerHTML = `本次搜尋共 ${count} 筆資料`;
     })
+    card.innerHTML = str;
+    searchCount.innerHTML = `本次搜尋共 ${count} 筆資料`;
 }
-// 新增套票資料
+
+// 驗證與新增套票資料
 function addTicketData(e) {
-    const ticketName = document.querySelector(".js-ticketName");
-    const ticketImg = document.querySelector(".js-ticketImg");
-    const ticketArea = document.querySelector(".js-ticketArea");
-    const ticketPrice = document.querySelector(".js-ticketPrice");
-    const ticketGroup = document.querySelector(".js-ticketGroup");
-    const ticketTag = document.querySelector(".js-ticketTag");
-    const ticketDescription = document.querySelector(".js-ticketDescription");
 
     // 金額、組數、星級的 type 轉為 Number
-    let ticketGroupValue = parseInt(ticketGroup.value);
     let ticketPriceValue = parseInt(ticketPrice.value);
+    let ticketGroupValue = parseInt(ticketGroup.value);
     let ticketTagValue = parseInt(ticketTag.value);
 
-    // 驗證數字 0-9
-    const reg = new RegExp("^[0-9]*$");
 
     if (ticketName.value == '') {
-        alert("套票名稱不可空白");
+        ticketName.focus();
+        verify[0].textContent = "套票名稱不可空白";
     } else if (ticketImg.value == '') {
-        alert("圖片網址不可空白");
+        ticketImg.focus();
+        verify[1].textContent = "圖片網址不可空白";
     } else if (ticketArea.value == '請選擇景點地區') {
-        alert("請選擇景點地區");
-    } else if (ticketPriceValue == '') {
-        alert("套票金額不可空白");
-    } else if (!reg.test(ticketPriceValue)) {
-        alert("套票金額請輸入數字");
-    } else if (ticketGroupValue == '') {
-        alert("套票組數不可空白");
-    } else if (!reg.test(ticketGroupValue)) {
-        alert("套票組數請輸入數字");
-    } else if (ticketTagValue == '' || !reg.test(ticketTagValue)) {
-        alert("套票星級不可空白");
-    } else if (ticketTagValue > 10 || !reg.test(ticketTagValue)) {
-        alert("星級區間為 1-10 分");
+        ticketArea.focus();
+        verify[2].textContent = "請選擇景點地區";
+    } else if (isNaN(ticketPriceValue)) {
+        ticketPrice.focus();
+        verify[3].textContent = "套票金額不可空白";
+    } else if (isNaN(ticketGroupValue)) {
+        ticketGroup.focus();
+        verify[4].textContent = "套票組數不可空白";
+    } else if (isNaN(ticketTagValue)) {
+        ticketTag.focus();
+        verify[5].textContent = "套票星級不可空白";
+    } else if (ticketTagValue > 10) {
+        ticketTag.focus();
+        verify[5].textContent = "星級區間為 1-10 分";
     } else if (ticketDescription.value == '') {
-        alert("套票描述不可空白");
+        ticketDescription.focus();
+        verify[6].textContent = "套票描述不可空白";
     } else if (ticketDescription.value.length >= 100) {
-        alert("字數不可超過 100 字");
+        ticketDescription.focus();
+        verify[6].textContent = "字數不可超過 100 字";
     } else {
         data.push({
-            "id": data.length,
-            "name": ticketName.value,
-            "imgUrl": ticketImg.value,
-            "area": ticketArea.value,
-            "description": ticketDescription.value,
-            "group": ticketGroupValue,
-            "price": ticketPriceValue,
-            "rate": ticketTagValue
+            id: data.length,
+            name: ticketName.value,
+            imgUrl: ticketImg.value,
+            area: ticketArea.value,
+            description: ticketDescription.value,
+            group: ticketGroupValue,
+            price: ticketPriceValue,
+            rate: ticketTagValue
         })
         // 下選單設定顯示全部地區
         selectArea.value = "全部地區";
-        // from.reset();
+        // 清空 input 欄位
+        form.reset();
         // 刷新畫面
         init();
-        // 清空 input 欄位
-        ticketName.value = "";
-        ticketImg.value = "";
-        ticketArea.value = "請選擇景點地區";
-        ticketDescription.value = "";
-        ticketGroup.value = "";
-        ticketPrice.value = "";
-        ticketTag.value = "";
     }
 }
 
+// 清除驗證文字
+function inputValue(e) {
+
+    if (ticketName.value !== '') {
+        verify[0].textContent = "";
+    }
+
+    if (ticketImg.value !== '') {
+        verify[1].textContent = "";
+    }
+
+    if (ticketArea.value !== '') {
+        verify[2].textContent = "";
+    }
+
+    if (ticketPrice.value !== '') {
+        verify[3].textContent = "";
+    }
+
+    if (ticketGroup.value !== '') {
+        verify[4].textContent = "";
+    }
+
+    if (ticketTag.value !== '') {
+        verify[5].textContent = "";
+    }
+
+    if (ticketDescription.value !== '') {
+        verify[6].textContent = "";
+    }
+}
+
+
+
+// 清除驗證文字
+form.addEventListener("input", inputValue, false);
+// 下拉選單篩選
 selectArea.addEventListener("change", selectFilter, false);
-addTicket.addEventListener("click", addTicketData, false)
+// 驗證與新增套票資料
+addTicket.addEventListener("click", addTicketData, false);
